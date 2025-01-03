4c895fed6ddc083dc133aa4fec6715fa17dbdf9a
Ahmed (Zougibe) Mohamed <84062148+zougibe@users.noreply.github.com>
2025-01-03 18:30:14 +0200

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,37 @@
+
+const images = [
+    "../../Client Side Tech/images/slide1.jpg",
+    "../../Client Side Tech/images/slide2.jpg",
+    "../../Client Side Tech/images/slide3.jpg",
+    "../../Client Side Tech/images/slide4.jpg"
+];
+
+let currentIndex = 0;
+
+
+$(".slider").click(function (e) {
+    const src = $(this).attr("src");
+    $("#mainP").attr("src", src); 
+    $(".mainPic").show(); 
+    currentIndex = images.indexOf(src);
+});
+
+
+$(".mainPic").click(function (e) {
+    if (e.target === this) {
+        $(".mainPic").hide(); 
+    }
+});
+
+
+$(".last").click(function () {
+    currentIndex = (currentIndex === 0) ? images.length - 1 : currentIndex - 1;
+    $("#mainP").attr("src", images[currentIndex]);
+});
+
+
+$(".next").click(function () {
+    currentIndex = (currentIndex + 1) % images.length;
+    $("#mainP").attr("src", images[currentIndex]);
+});
+
